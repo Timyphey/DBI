5a865f936b92f5fbcae3353d4eb03a02e946421a
Timyphey <timmelgamer@gmail.com>
2023-11-14 01:45:20 +0100

    sqlalchemytest verbindung funktioniert

---
+++ sqlalchemytest.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from sqlalchemy import text
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:J4p4nr3is32015!@127.0.0.1:3306/dbi')
+
+conn = engine.connect()
+
+#pid_input = input("Enter the pid: ")
+
+res = conn.execute(text("SELECT agents.aid, agents.aname, sum(dollars) as sumdollar FROM orders, agents WHERE pid='p01' and orders.aid = agents.aid GROUP BY agents.aid, agents.aname ORDER BY sumdollar DESC;")).fetchall()
+
+conn.close()
+
+print(res)
+
+#for row in res:
+#    aid = row[0]
+#    aname = row[1]
+#    total_dollars = row[2]
+#
+#    print(f"{aid}\t{aname}\t{total_dollars}")
