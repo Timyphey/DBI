13c4bb311b10ba0fa856534edbc8ba7612e82a77
Timyphey <timmelgamer@gmail.com>
2023-11-14 18:30:04 +0100

    added time tracking

--- Praktiumsaufgabe 7/main.py
+++ Praktiumsaufgabe 7/main.py
@@ -1,13 +1,15 @@
-#import configparser
+import configparser
 import sqlalchemy
 from sqlalchemy import text
 import random 
+import time	
 
 engine = sqlalchemy.create_engine('mysql+pymysql://root:J4p4nr3is32015!@127.0.0.1:3306/Benchmark_dbi')
 
 
 def create_tupel(n):
     conn = engine.connect()
+    start_time = time.time() 
     #if conn.is_connected():
     for x in range(1,n+1):
 
@@ -16,7 +18,7 @@ def create_tupel(n):
         create_branchname_tupel= f"""
         
         insert into Benchmark_dbi.branches
-            values({x},"Sparkasse Rhein-Main", 0, "Branches der Sparkasse Rhein-Main in der Innenstadt Adresse mit 72 Chars")
+            values({x},"Sparkasse Rhein-Main", 0, "Branches der Sparkasse Rhein-Main in der Innenstadt Adresse mit 72 Chars");
 
         """
 
@@ -29,25 +31,31 @@ def create_tupel(n):
         create_accounts_tupel= f"""
 
         insert into Benchmark_dbi.accounts
-            values({x},"Acc Name mit 20 Char", 0, {random_branchid},"Account von vielen coolen hunderttausend Kunden Adresse mit 68 Chars")
+            values({x},"Acc Name mit 20 Char", 0, {random_branchid},"Account von vielen coolen hunderttausend Kunden Adresse mit 68 Chars");
 
         """
         conn.execute(text(create_accounts_tupel))            
 
         conn.commit()
+        
+        #if x%10000 ==0: print(x, "accs created")
 
     for x in range(1,n*10+1):
         random_branchid= random.randint(1,n)
         create_tellers_tupel= f"""
 
         insert into Benchmark_dbi.tellers
-            values({x},"Tel Name mit 20 Char", 0, {random_branchid},"bester Teller der nicen Sparkasse der Innenstadt Adresse mit 68 Char")
+            values({x},"Tel Name mit 20 Char", 0, {random_branchid},"bester Teller der nicen Sparkasse der Innenstadt Adresse mit 68 Char");
 
         """
         conn.execute(text(create_tellers_tupel))
 
         conn.commit()
-        
+    
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Time taken to create with n={n}:    {elapsed_time} seconds")
+    
     conn.close()
     
 
